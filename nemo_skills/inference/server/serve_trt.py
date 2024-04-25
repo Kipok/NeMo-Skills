ea7147cbe7d312930019aca21198e63da59ef5af
gnalbandyan <gnalbandyan@nvidia.com>
2024-04-25 10:49:07 +0400

    Minor Fix

--- nemo_skills/inference/server/serve_trt.py
+++ nemo_skills/inference/server/serve_trt.py
@@ -95,6 +95,7 @@ def parse_input(input_texts: str, tokenizer):
     batch_input_ids = [
         tokenizer.encode(
             input_text,
+            add_special_tokens=False,
         )
         for input_text in input_texts
     ]
