532578120f1b56f29ab7cf57507098866ff2f803
Igor Gitman <igitman@nvidia.com>
2024-05-27 10:51:39 -0700

    Add nemo generation test
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- tests/test_generation.py
+++ tests/test_generation.py
@@ -69,3 +69,45 @@ python pipeline/run_eval.py \
     assert wrong_answer == 45.0
     assert no_answer == 15.0
     assert total == 20
+
+
+def test_nemo_run_eval():
+    model_path = os.getenv('NEMO_SKILLS_TEST_NEMO_MODEL')
+    if not model_path:
+        pytest.skip("Define NEMO_SKILLS_TEST_NEMO_MODEL to run this test")
+    output_path = os.getenv('NEMO_SKILLS_TEST_OUTPUT', '/tmp')
+
+    cmd = f""" \
+python pipeline/run_eval.py \
+    --model_path {model_path} \
+    --server_type nemo \
+    --output_dir {output_path} \
+    --benchmarks gsm8k:0 \
+    --num_gpus 2 \
+    --num_nodes 1 \
+    +prompt=code_base \
+    ++prompt.few_shot_examples.examples_type=gsm8k_text_with_code \
+    ++prompt.few_shot_examples.num_few_shots=5 \
+    ++split_name=test \
+    batch_size=8 \
+    max_samples=20 \
+"""
+    subprocess.run(
+        cmd,
+        shell=True,
+    )  # not checking the error as it's expected to finish with non-zero error code
+
+    # double checking that code was actually executed
+    with open(f"{output_path}/gsm8k/output-greedy.jsonl") as fin:
+        data = [json.loads(line) for line in fin]
+
+    for elem in data:
+        assert '<llm-code>' in elem['generated_solution']
+        assert elem['error_message'] != '<not_executed>'
+
+    # running compute_metrics to check that results are expected
+    correct_answer, wrong_answer, no_answer, total = compute_metrics([f"{output_path}/gsm8k/output-greedy.jsonl"])
+    assert correct_answer == 40.0
+    assert wrong_answer == 45.0
+    assert no_answer == 15.0
+    assert total == 20
