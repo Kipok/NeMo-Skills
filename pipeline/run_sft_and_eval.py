41617de1a52a174a0d3b4e0fe241ce83d102de15
Jocelyn Huang <jocelynh@nvidia.com>
2024-05-29 14:30:59 -0700

    Remove debug prints
    Signed-off-by: Jocelyn Huang <jocelynh@nvidia.com>

--- pipeline/run_sft_and_eval.py
+++ pipeline/run_sft_and_eval.py
@@ -26,22 +26,11 @@ from nemo_skills.utils import setup_logging
 
 
 def start_job(extra_sbatch_args: str, cmd: str) -> int:
-    print("--------------")
     full_cmd = f'{extra_sbatch_args} {cmd}'
-    print(full_cmd)
-    print("===========")
 
     if CLUSTER_CONFIG['cluster'] == 'local':
         subprocess.run(full_cmd, shell=True)
         return 0
-    try:
-        job_id = subprocess.run(full_cmd, shell=True, check=True, capture_output=True).stdout.decode()
-    except Exception as e:
-        print("ERRROR INFO vvvvvvvvvvvvvvvvvvvvv")
-        print(e.returncode)
-        print(e.output.decode())
-        stderr=subprocess.STDOUT
-        return -1
     print(f"Submitted batch job(s) {job_id}")
     return job_id
 
