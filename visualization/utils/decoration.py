69366297b95c31cb133d2e58f3ff06c6127021f2
Daria Gitman <dgitman@Darias-MacBook-Pro.local>
2024-06-17 18:12:29 +0400

    fix latex rendering

--- visualization/utils/decoration.py
+++ visualization/utils/decoration.py
@@ -15,7 +15,7 @@
 import random
 import re
 import string
-from typing import Dict
+from typing import Callable, Dict, Optional, Tuple
 
 from ansi2html import Ansi2HTMLConverter
 from dash import dcc, html
@@ -24,7 +24,138 @@ from pygments.formatters import HtmlFormatter
 from pygments.lexers import PythonLexer
 
 
-def design_text_output(text: str, style={}):
+def get_starts_with_tag_function(
+    tag: str, default_index_move: int
+) -> Callable[[str, int], Tuple[bool, int]]:
+    def starts_with_tag_func_templ(text: str, index: int):
+        is_starts_with_tag = text.startswith(tag, index)
+        if not is_starts_with_tag:
+            returning_index = index + default_index_move
+        elif '{' not in tag:
+            returning_index = index + len(tag)
+        else:
+            returning_index = text.find('}', index) % (len(text) + 1) + 1
+
+        return is_starts_with_tag, returning_index
+
+    return starts_with_tag_func_templ
+
+
+def proccess_tag(
+    text: str,
+    start_index: int,
+    detect_start_token: Callable[[str, int], Tuple[bool, int]],
+    detect_end_token: Callable[[str, int], Tuple[bool, int]],
+    end_sign: Optional[str],
+    last_block_only: bool = False,
+) -> int:
+    count = 0
+    index = start_index
+    while index < len(text):
+        if end_sign and text[index] == end_sign:
+            return start_index, start_index + 1
+        is_start_token, new_index = detect_start_token(text, index)
+        count += is_start_token
+        if last_block_only and is_start_token:
+            start_index = index
+            count = min(1, count)
+        index = new_index
+        is_end_token, index = detect_end_token(text, index)
+        count -= is_end_token
+        if count == 0:
+            break
+    return start_index, index + 1
+
+
+def get_single_dollar_functions(
+    direction: int, default_index_move: int
+) -> Callable[[str, int], Tuple[bool, int]]:
+    return lambda text, index: (
+        text[index] == '$'
+        and text[index - direction].isspace()
+        and not text[index + direction].isspace(),
+        index + default_index_move,
+    )
+
+
+def get_detection_functions(text, index) -> tuple[
+    Callable[[str, int], Tuple[bool, int]],
+    Callable[[str, int], Tuple[bool, int]],
+    Optional[str],
+    bool,
+    bool,
+]:
+    multiline_tags = [('\\begin{', '\\end{', True), ('$$', '$$', False)]
+    for start_tag, end_tag, add_dollars in multiline_tags:
+        if text.startswith(start_tag, index):
+            return (
+                get_starts_with_tag_function(start_tag, 1),
+                get_starts_with_tag_function(end_tag, 0),
+                None,
+                add_dollars,
+                False,
+            )
+
+    starts_with_dollar_func = get_single_dollar_functions(1, 1)
+    ends_with_dollar_func = get_single_dollar_functions(-1, 0)
+    if starts_with_dollar_func(text, index)[0]:
+        return starts_with_dollar_func, ends_with_dollar_func, '\n', False, True
+
+    return None, None, None, None, None
+
+
+def proccess_plain_text(text: str) -> str:
+    special_chars = r'*_{}[]()#+-.!'
+    for character in special_chars:
+        text = text.replace(character, '\\' + character)
+    return text
+
+
+def preprocess_latex(text: str) -> str:
+    text = '\n' + text.replace('\\[', '\n$$\n').replace('\\]', '\n$$\n') + '\n'
+    index = 1
+    texts = []
+    start_plain_text_index = -1
+    while index < len(text) - 1:
+        (
+            detect_start_token,
+            detect_end_token,
+            end_sign,
+            add_dollars,
+            use_last_block_only,
+        ) = get_detection_functions(text, index)
+        if detect_start_token is not None:
+            if start_plain_text_index != -1:
+                texts.append(proccess_plain_text(text[start_plain_text_index:index]))
+                start_plain_text_index = -1
+
+            start_index, new_index = proccess_tag(
+                text,
+                index,
+                detect_start_token,
+                detect_end_token,
+                end_sign,
+                use_last_block_only,
+            )
+            texts.append(proccess_plain_text(text[index:start_index]))
+            if add_dollars:
+                texts.append('\n$$\n')
+                texts.append(text[start_index:new_index].strip())
+                texts.append('\n$$\n')
+            else:
+                texts.append(text[start_index:new_index])
+            index = new_index
+        elif start_plain_text_index == -1:
+            start_plain_text_index = index
+            index += 1
+        else:
+            index += 1
+    if start_plain_text_index != -1:
+        texts.append(proccess_plain_text(text[start_plain_text_index:]))
+    return ''.join(texts).strip()
+
+
+def design_text_output(text: str, style={}) -> html.Div:
     conv = Ansi2HTMLConverter()
     ansi_escape = re.compile(r'\x1b\[[0-9;]*m')
 
@@ -46,61 +177,6 @@ def design_text_output(text: str, style={}):
             ),
             style=style,
         )
-
-    def preprocess_latex(text):
-        text = text.replace('\\[', '\n$$\n').replace('\\]', '\n$$\n')
-
-        special_chars = set(r'*_{}[]()#+-.!')
-        begin_tag = '\\begin'
-        end_tag = '\\end'
-        begin_tag_index = -1
-        math_block_flags = {'$$': False, '$': False}
-        begin_end_counter = 0
-        index = 0
-        string_modifications = [(0, "")]
-
-        def handle_braces(index):
-            while index < len(text) and text[index] == '{':
-                while index < len(text) and text[index] != '}':
-                    index += 1
-                if index < len(text) - 1 and text[index + 1] == '{':
-                    index += 1
-            return index
-
-        while index < len(text):
-            if text.startswith(begin_tag, index):
-                if begin_end_counter == 0:
-                    begin_tag_index = index
-                begin_end_counter += 1
-                index += len(begin_tag)
-                index = handle_braces(index)
-            elif text.startswith(end_tag, index):
-                begin_end_counter -= 1
-                index += len(end_tag)
-                index = handle_braces(index)
-                if begin_end_counter == 0:
-                    string_modifications.append((begin_tag_index, "\n$$\n"))
-                    string_modifications.append((index + 1, "\n$$\n"))
-            elif text.startswith("$$", index):
-                math_block_flags['$$'] = not math_block_flags['$$']
-                index += 1
-            elif text[index] == "$":
-                math_block_flags['$'] = not math_block_flags['$']
-            elif not any(math_block_flags.values()) and begin_end_counter == 0:
-                if text[index] in special_chars:
-                    string_modifications.append((index, "\\"))
-            index += 1
-
-        string_pieces = []
-        string_modifications.sort()
-        for i in range(len(string_modifications)):
-            if i != 0:
-                start, end = string_modifications[i - 1][0], string_modifications[i][0]
-                string_pieces.append(text[start:end])
-                string_pieces.append(string_modifications[i][1])
-        string_pieces.append(text[string_modifications[-1][0] :])
-
-        return "".join(string_pieces)
 
     return html.Div(
         dcc.Markdown(preprocess_latex(text), mathjax=True, dangerously_allow_html=True),
