25a9b2f82960f24d51a3752a036650f328b34a12
Igor Gitman <igitman@nvidia.com>
2024-06-28 16:00:36 -0700

    Remove unneeded preprocessing
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- nemo_skills/evaluation/code_utils.py
+++ nemo_skills/evaluation/code_utils.py
@@ -16,45 +16,6 @@ def preprocess_code(generation_dict: dict):
             print(completion)
             print("================\n")
 
-    if "__name__ == \"__main__\"" in completion:
-        next_line = completion.index('if __name__ == "__main__":')
-        completion = completion[:next_line].strip()
-        # print(completion)
-
-    if "# Example usage" in completion:
-        # print(completion)
-        next_line = completion.index('# Example usage')
-        completion = completion[:next_line].strip()
-
-    if "[/CODE]" in completion:
-        next_line = completion.index('[/CODE]')
-        completion = completion[:next_line].strip()
-
-    if "[SOL]" in completion:
-        next_line = completion.index('[SOL]')
-        completion = completion[:next_line].strip()
-
-        if "[/SOL]" in completion:
-            next_line = completion.index('[/SOL]')
-            completion = completion[:next_line].strip()
-
-    if "[/INST]" in completion:
-        next_line = completion.index('[/INST]')
-        completion = completion[:next_line].strip()
-
-    if "Explanation:" in completion:
-        next_line = completion.index('Explanation:')
-        completion = completion[:next_line].strip()
-
-    if "<extra_id_1>" in completion:
-        next_line = completion.index('<extra_id_1>')
-        completion = completion[:next_line].strip()
-
-    if "here" in completion.lower() and "code" in completion.lower() and "def" in completion:
-        # Find first def after "Here is the" and remove everything before that
-        next_line = completion.index('def')
-        completion = completion[next_line:].strip()
-
     if completion.startswith(" "):
         completion = completion.strip()
 
