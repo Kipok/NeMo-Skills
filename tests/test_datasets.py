2f8de091663c36da8f5d432110f379f6e9e77a8d
wedu-nvidia <wedu@nvidia.com>
2024-07-17 12:43:25 -0700

    add two benchmark datasets aime-2024 and math-odyssey (#64)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Igor Gitman <igitman@nvidia.com>

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -65,6 +65,8 @@ def test_data_scripts():
         'mmlu/dev.jsonl',
         'mmlu/val.jsonl',
         'ifeval/test.jsonl',
+        'math-odyssey/test.jsonl',
+        'aime-2024/test.jsonl',
     ]
     for file in expected_files:
         assert (Path(__file__).absolute().parents[1] / "datasets" / file).exists()
