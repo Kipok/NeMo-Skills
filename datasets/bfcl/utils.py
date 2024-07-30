cdc3d9de8e0541c91ca992b349cd9c28be14580a
Shubham Toshniwal <shtoshni@gmail.com>
2024-07-29 23:03:06 -0400

    Update datasets/bfcl/utils.py
    Co-authored-by: Igor Gitman <igitman@nvidia.com>

--- datasets/bfcl/utils.py
+++ datasets/bfcl/utils.py
@@ -80,7 +80,7 @@ def augment_prompt_by_language(prompt, test_category):
 
 # Adapted from here - https://github.com/ShishirPatil/gorilla/blob/main/berkeley-function-call-leaderboard/model_handler/utils.py
 def language_specific_pre_processing(function, test_category):
-    if type(function) is dict:
+    if isinstance(function, dict):
         function = [function]
     if len(function) == 0:
         return function
