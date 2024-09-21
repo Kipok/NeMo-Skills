41ddb6949f5ab3234a39005910ac32ffb157e50c
Igor Gitman <igitman@nvidia.com>
2024-09-20 23:21:26 -0700

    Switch to afterany dependency
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- nemo_skills/pipeline/utils.py
+++ nemo_skills/pipeline/utils.py
@@ -273,6 +273,7 @@ def get_executor(
         wait_time_for_group_job=0.01,
         monitor_group_job_wait_time=20,
         dependencies=dependencies,
+        dependency_type="afterany",
     )
 
 
