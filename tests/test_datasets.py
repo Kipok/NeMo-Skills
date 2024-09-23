304a7e33074ae4a208a9688b57bbf52351cb3752
Igor Gitman <igitman@nvidia.com>
2024-09-23 11:58:58 -0700

    Fix test_datasets.py
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -29,7 +29,7 @@ DATASETS = [
     ('gsm-plus', ['test', 'test_rounded']),
     ('gsm8k', ['train', 'train_full', 'validation', 'test']),
     ('human-eval', ['test']),
-    ('IFMetrics', ['test']),
+    ('ifeval', ['test']),
     ('math', ['train', 'train_full', 'validation', 'test']),
     ('math-odyssey', ['test']),
     ('mawps', ['test']),
