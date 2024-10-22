8abc60e674f1d58233f2274c215458c2082c6ca2
arendu <adithya.r@gmail.com>
2024-10-22 14:14:38 -0700

    remove pdb
    Signed-off-by: arendu <adithya.r@gmail.com>

--- nemo_skills/pipeline/utils.py
+++ nemo_skills/pipeline/utils.py
@@ -518,8 +518,6 @@ def add_task(
     if server_config is not None:
         server_cmd, num_server_tasks = get_server_command(**server_config, cluster_config=cluster_config)
         if 'container' not in server_config:
-            import pdb
-            pdb.set_trace()
             server_container = cluster_config["containers"][server_config['server_type']]
         server_executor = get_executor(
             cluster_config=cluster_config,
