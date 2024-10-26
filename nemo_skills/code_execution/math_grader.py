eec41c7ea2ffaebbbc72ed977b01d9a6fff383c4
arendu <adithya.r@gmail.com>
2024-10-25 19:28:54 -0700

    debug
    Signed-off-by: arendu <adithya.r@gmail.com>

--- nemo_skills/code_execution/math_grader.py
+++ nemo_skills/code_execution/math_grader.py
@@ -546,8 +546,8 @@ def symbolic_equal(a, b, tolerance, timeout=10.0):
 def extract_answer(string: str, extract_from_boxed: bool = True, extract_regex: str = r"The final answer is (.+)$"):
     """Extract Answer String from \\boxed expression or based on regex"""
     if not extract_from_boxed:
-        print(extract_regex, "is the regex")
         match = re.search(extract_regex, string)
+        print(extract_regex, "is the regex", match, "match")
         if match:
             return match.group(0)
         return None
