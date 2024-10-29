1e48f0e12bbe42327d8c6dbaa533658c0dcda39f
Igor Gitman <igitman@nvidia.com>
2024-10-29 15:59:58 -0700

    Change conversion tests
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- tests/gpu-tests/run_llama.sh
+++ tests/gpu-tests/run_llama.sh
@@ -6,27 +6,28 @@
 set -e
 
 export NEMO_SKILLS_TEST_HF_MODEL=/mnt/datadrive/nemo-skills-test-data/Meta-Llama-3.1-8B-Instruct
+export NEMO_SKILLS_TEST_MODEL_TYPE=llama
 
-# # first running the conversion tests
-# pytest tests/gpu-tests/test_convert.py -k test_hf_trtllm_conversion -s -x
-# export NEMO_SKILLS_TEST_TRTLLM_MODEL=/tmp/nemo-skills-tests/conversion/hf-to-trtllm/model
-# pytest tests/gpu-tests/test_convert.py -k test_hf_nemo_conversion -s -x
-# export NEMO_SKILLS_TEST_NEMO_MODEL=/tmp/nemo-skills-tests/conversion/hf-to-nemo/model
-# pytest tests/gpu-tests/test_convert.py -k test_nemo_hf_conversion -s -x
-# # using the back-converted model to check that it's reasonable
-# export NEMO_SKILLS_TEST_HF_MODEL=/tmp/nemo-skills-tests/conversion/nemo-to-hf/model
+# first running the conversion tests
+pytest tests/gpu-tests/test_convert.py -k test_hf_trtllm_conversion -s -x
+export NEMO_SKILLS_TEST_TRTLLM_MODEL=/tmp/nemo-skills-tests/conversion/hf-to-trtllm/model
+pytest tests/gpu-tests/test_convert.py -k test_hf_nemo_conversion -s -x
+export NEMO_SKILLS_TEST_NEMO_MODEL=/tmp/nemo-skills-tests/conversion/hf-to-nemo/model
+pytest tests/gpu-tests/test_convert.py -k test_nemo_hf_conversion -s -x
+# using the back-converted model to check that it's reasonable
+export NEMO_SKILLS_TEST_HF_MODEL=/tmp/nemo-skills-tests/conversion/nemo-to-hf/model
 
-# # generation/evaluation tests
-# pytest tests/gpu-tests/test_eval.py -s -x
-# pytest tests/gpu-tests/test_generate.py -s -x
+# generation/evaluation tests
+pytest tests/gpu-tests/test_eval.py -s -x
+pytest tests/gpu-tests/test_generate.py -s -x
 
-# for sft we are using the tiny random llama model to run much faster
+# for sft we are using the tiny random model to run much faster
 docker run --rm \
     -e HF_TOKEN=$HF_TOKEN \
     -v /tmp:/tmp \
     -v `pwd`:/nemo_run/code \
     igitman/nemo-skills-nemo:0.4.2 \
-    python /nemo_run/code/tests/gpu-tests/make_tiny_llm.py --model_type qwen
+    python /nemo_run/code/tests/gpu-tests/make_tiny_llm.py --model_type $NEMO_SKILLS_TEST_MODEL_TYPE
 
 # converting the model through test
 export NEMO_SKILLS_TEST_HF_MODEL=/tmp/nemo-skills-tests/tiny-qwen-hf
