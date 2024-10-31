e8e0898d6582bef5df4a2dcca740220099b9ae55
Igor Gitman <igitman@nvidia.com>
2024-10-31 15:21:27 -0700

    Allow not passing template for other servers
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- nemo_skills/inference/generate.py
+++ nemo_skills/inference/generate.py
@@ -103,8 +103,15 @@ class GenerateSolutionsConfig:
         if self.dataset is None and self.prompt_config is None:
             raise ValueError("If `dataset` is not provided, `prompt_config` is required")
 
-        if self.server["server_type"] != "openai" and self.prompt_template is None:
-            raise ValueError("Prompt template is required for non-OpenAI servers")
+        if self.server["server_type"] == "trtllm" and self.prompt_template is None:
+            # TODO: fix that
+            raise ValueError("Prompt template is required for trtllm servers")
+
+        if self.server["server_type"] == "nemo" and self.prompt_template is not None:
+            LOG.warning(
+                "NeMo implementation of openai chat completions api doesn't support batching and thus is very slow. "
+                "Until this is fixed, we highly recommend that you provide prompt template explicitly."
+            )
 
         if self.server["server_type"] == "openai" and self.prompt_template is not None:
             raise ValueError("Prompt template is not supported for OpenAI server")
@@ -119,6 +126,14 @@ def generate(cfg: GenerateSolutionsConfig):
     cfg = GenerateSolutionsConfig(_init_nested=True, **cfg)
 
     LOG.info("Config used: %s", cfg)
+
+    if cfg.prompt_template is None:
+        # TODO: handle this explicitly in model.py clients
+        # switching to OpenAI client always if prompt template is not provided
+        cfg.server.server_type = "openai"
+        if cfg.code_execution:
+            raise ValueError("Code execution is not supported for OpenAI server")
+
     if cfg.code_execution:
         sandbox = get_sandbox(**cfg.sandbox) if cfg.sandbox is not None else None
         llm = get_code_execution_model(**cfg.server, sandbox=sandbox)
