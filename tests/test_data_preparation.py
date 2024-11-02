1271082ad472ec8168e9732e12f0ea66a5ca66f1
stoshniwal <shtoshni@gmail.com>
2024-11-02 16:21:30 -0700

    Minor fixes

--- tests/test_data_preparation.py
+++ tests/test_data_preparation.py
@@ -33,7 +33,7 @@ def test_multiple_files():
         f"    ++prompt_config=generic/math "
         f"    ++prompt_template=llama3-instruct "
         f"    ++exclude_optional_keys=false "
-        f"    ++hf_model_name='meta-llama/Meta-Llama-3.1-8B'"
+        f"    ++hf_model_name='meta-llama/Meta-Llama-3.1-8B' "
         f"    ++filters.drop_multi_boxed=true "
         f"    ++filters.trim_solutions=true "
         f"    ++filters.drop_incorrect_arithmetic=false "
@@ -62,7 +62,7 @@ def test_exclude_keys():
         f"    ++prompt_config=generic/math "
         f"    ++prompt_template=llama3-instruct "
         f"    ++exclude_optional_keys=true "
-        f"    ++hf_model_name='meta-llama/Meta-Llama-3.1-8B'"
+        f"    ++hf_model_name='meta-llama/Meta-Llama-3.1-8B' "
         f"    ++filters.drop_multi_boxed=true "
         f"    ++filters.trim_solutions=true "
         f"    ++filters.drop_incorrect_arithmetic=false "
@@ -92,7 +92,7 @@ def test_code_sft_data():
         f"    ++prompt_config=generic/codegen "
         f"    ++prompt_template=llama3-instruct "
         f"    ++exclude_optional_keys=false "
-        f"    ++hf_model_name='meta-llama/Meta-Llama-3.1-8B'"
+        f"    ++hf_model_name='meta-llama/Meta-Llama-3.1-8B' "
         f"    ++filters.drop_incorrect_code_blocks=false "
         f"    ++generation_suffix='\"<|eot_id|>\"' ",
         check=True,
