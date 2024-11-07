5a8b5ff3605c4789e652246a64fa8d89f956e8c5
gwarmstrong <georgea@nvidia.com>
2024-11-06 16:42:23 -0800

    MAINT update cmdline comment format

--- nemo_skills/inference/reward_model.py
+++ nemo_skills/inference/reward_model.py
@@ -43,16 +43,15 @@ class RewardModelConfig:
 
     input_file: str | None = None  # Can directly specify an input file, if using a custom dataset
     output_file: str | None = None  # Where to save the generations if `input_file` is provided
-    input_dir: str | None = (
-        None  # Can specify an input directory, where the file will be inferred output-greedy.jsonl if no seed is provided, and output-rs{{seed}}.jsonl. This pattern is used to match the output files from the `generate` pipeline
-    )
-    output_dir: str | None = (
-        None  # Where to save the generations (with the identical file name) if `input_dir` is provided
-    )
-    random_seed: str | None = (
-        None  # Used to identify the input file if `input_dir` is provided. If `random_seed` is not provided, the input will be assumed to be from 'greedy' generation
-    )
-
+    # Can specify an input directory, where the file will be inferred output-greedy.jsonl if no seed
+    # is provided, and output-rs{{seed}}.jsonl. This pattern is used to match the output files from
+    # the `generate` pipeline
+    input_dir: str | None = None
+    # Where to save the generations (with the identical file name) if `input_dir` is provided
+    output_dir: str | None = None
+    # Used to identify the input file if `input_dir` is provided. If `random_seed` is not provided,
+    # the input will be assumed to be from 'greedy' generation
+    random_seed: str | None = None
     batch_size: int = 128
     max_samples: int = -1  # If > 0, will stop after generating this many samples. Useful for debugging
     skip_filled: bool = False  # If True, will skip the generations that are already in the output file
