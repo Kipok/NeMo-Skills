4c5be59729733eb1c8a028de8edec0ffa051baf1
stoshniwal <shtoshni@gmail.com>
2024-11-10 19:47:27 -0800

    Fix embedding access

--- nemo_skills/inference/server/model.py
+++ nemo_skills/inference/server/model.py
@@ -529,8 +529,8 @@ class VLLMModel(BaseModel):
     def get_rm_score(self, prompts: list[str | dict]):
         """Get Reward Model Scores."""
         responses = self.oai_client.embeddings.create(input=prompts, model=self.model)
-        # LOG.info([len(seq_scores) for seq_scores in responses.data])
-        outputs = [{'score': seq_scores[-1]} for seq_scores in responses.data]
+        LOG.info([len(data.embedding) for data in responses.data])
+        outputs = [{'score': data.embedding[-1]} for data in responses.data]
         return outputs
 
     def prompt_api(
