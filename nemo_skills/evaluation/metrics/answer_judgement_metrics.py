298fb9f47732699c0f0a6d4eb90d616e368b2126
stoshniwal <shtoshni@gmail.com>
2024-11-27 14:49:55 -0800

    Updates answer_judgement metrics calculation

--- nemo_skills/evaluation/metrics/answer_judgement_metrics.py
+++ nemo_skills/evaluation/metrics/answer_judgement_metrics.py
@@ -28,6 +28,11 @@ class AnswerJudgementMetrics(BaseMetrics):
     def is_incomplete(self, elem):
         return 'judgement' not in elem or 'expected_judgement' not in elem
 
+    def update_perf_dict(self, perf_dict, is_correct, is_fp, is_fn):
+        perf_dict["total_correct"] += int(is_correct)
+        perf_dict["fp_count"] += int(is_fp)
+        perf_dict["fn_count"] += int(is_fn)
+
     def update(self, predictions):
         """Updating the evaluation results with the current element.
 
@@ -37,7 +42,8 @@ class AnswerJudgementMetrics(BaseMetrics):
         """
         self.total += 1
         if len(predictions) > 1:
-            is_correct, fp_count, fn_count = False, False, False
+            # Pass@k
+            is_correct, is_fp, is_fn = False, False, False
             is_correct = any(
                 [
                     is_correct_judgement(elem['judgement']) == is_correct_judgement(elem['expected_judgement'])
@@ -47,45 +53,50 @@ class AnswerJudgementMetrics(BaseMetrics):
 
             if not is_correct:
                 if is_correct_judgement(predictions[0]['judgement']):
-                    self.fp_count += 1
+                    is_fp = True
                 else:
-                    self.fn_count += 1
+                    is_fn = True
+
+            self.update_perf_dict(self.agg_mode_dict[f"pass@{len(predictions)}"], is_correct, is_fp, is_fn)
+
+            # Majority@k
+            # Reinitialize local vars
+            is_correct, is_fp, is_fn = False, False, False
 
             answers = [is_correct_judgement(elem['judgement']) for elem in predictions]
             majority_judgement = Counter(answers).most_common(1)[0]
             is_correct = majority_judgement == is_correct_judgement(predictions[0]['expected_judgement'])
-            self.total_correct += is_correct
+
             if not is_correct:
                 if majority_judgement:
-                    self.fp_count += 1
+                    is_fp = True
                 else:
-                    self.fn_count += 1
+                    is_fn = True
+
+            self.update_perf_dict(self.agg_mode_dict[f"majority@{len(predictions)}"], is_correct, is_fp, is_fn)
         else:
             is_correct = is_correct_judgement(predictions[0]['judgement']) == is_correct_judgement(
                 predictions[0]['expected_judgement']
             )
-            self.total_correct += is_correct
             if not is_correct:
                 if is_correct_judgement(predictions[0]['judgement']):
-                    self.fp_count += 1
+                    is_fp = True
                 else:
-                    self.fn_count += 1
+                    is_fn = True
+
+            self.update_perf_dict(self.agg_mode_dict[f"greedy"], is_correct, is_fp, is_fn)
 
     def get_metrics(self):
         metrics_dict = {}
         for agg_mode, agg_metric_dict in self.agg_mode_dict.items():
             metrics_dict[agg_mode] = {"num_entries": self.total}
 
-        return {
-            "num_entries": self.total,
-            "correct_judgements": self.total_correct / self.total * 100.0,
-            "false_positives": self.fp_count / self.total * 100.0,
-            "false_negatives": self.fn_count / self.total * 100.0,
-        }
+            metrics_dict[agg_mode]["correct_judgements"] = (agg_metric_dict["total_correct"] / self.total) * 100.0
+            metrics_dict[agg_mode]["false_positives"] = (agg_metric_dict["fp_count"] / self.total) * 100.0
+            metrics_dict[agg_mode]["false_negatives"] = (agg_metric_dict["fn_count"] / self.total) * 100.0
+
+        return metrics_dict
 
     def reset(self):
-        # self.total_correct = 0
-        # self.fp_count = 0
-        # self.fn_count = 0
         self.total = 0
         self.agg_mode_dict = defaultdict(lambda: defaultdict(int))
