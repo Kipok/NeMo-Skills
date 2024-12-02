e09ca3eae05d1d7556fcaf7adaf8301cf7bcaf98
jubick1337 <mattyson.so@gmail.com>
2024-12-02 11:36:53 -0800

    fix import

--- nemo_skills/dataset/mmlu_pro/__init__.py
+++ nemo_skills/dataset/mmlu_pro/__init__.py
@@ -11,9 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from nemo_skills.evaluation.metrics import MathMetrics
+
 
 # settings that define how evaluation should be done by default (all can be changed from cmdline)
+from nemo_skills.evaluation.metrics.math_metrics import MathMetrics
+
 PROMPT_CONFIG = 'generic/multichoice'
 DATASET_GROUP = 'multichoice'
 METRICS_CLASS = MathMetrics  # TODO: update this?
