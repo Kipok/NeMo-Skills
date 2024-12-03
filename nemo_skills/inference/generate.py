14febf64862b9a72ec382f11aed132f7b4bfd82c
Igor Gitman <igitman@nvidia.com>
2024-12-03 09:05:55 -0800

    Update nemo_skills/inference/generate.py

--- nemo_skills/inference/generate.py
+++ nemo_skills/inference/generate.py
@@ -256,7 +256,7 @@ def generate(cfg: GenerateSolutionsConfig):
                                 prompt.fill(turn_data_points[dp_index], multi_turn_key=cfg.multi_turn_key)
                                 for dp_index in dp_indices
                             ],
-                            stop_phrases = combine_stop_phrases(prompt.stop_phrases, extra_stop_phrases),
+                            stop_phrases=combine_stop_phrases(prompt.stop_phrases, extra_stop_phrases),
                             **asdict(cfg.inference),
                             **extra_generate_params,
                         )
